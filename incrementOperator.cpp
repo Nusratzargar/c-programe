9b7e9574ea577b1c26950791f176a1d3160ba613
Nusratzargar <96786493+Nusratzargar@users.noreply.github.com>
2022-01-04 18:31:33 +0530

    Add files via upload

---
+++ incrementOperator.cpp
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+   int a=10,b;
+   b=++a;
+   printf("\n After prefix Increament :");
+   printf("a=%d and b=%d",a,b);
+   a=10;
+   b=a++;
+   printf("\n After postfix Increament :");
+   printf("a=%d and b=%d",a,b);
+   
+   return 0;
+}
