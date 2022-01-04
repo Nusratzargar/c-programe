93815bf9e435caa6bc7dddb8a3154b075659bcc4
Nusratzargar <96786493+Nusratzargar@users.noreply.github.com>
2022-01-04 18:33:30 +0530

    Add files via upload

---
+++ istprogram.cpp
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<conio.h>
+ int main ()
+ {
+     int a,b,c;
+	printf("give me first number");
+	scanf("%d",&a);
+	printf("give me second");
+	scanf("%d",&b);
+	 c=a+b;
+	printf("sum of two numbers is=%d",c);
+	getch();
+}
