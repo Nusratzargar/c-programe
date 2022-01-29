f8b8a82a47b0111f8293ac910c85e6a30c0d049f
Nusratzargar <96786493+Nusratzargar@users.noreply.github.com>
2022-01-29 11:55:41 +0530

    Add files via upload

---
+++ SerachAnElementInArray.cpp
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+	int a[20],n,i,x,f=0;
+	printf("Enter the no.of elements of your intrest but not more than 20\n");
+	scanf("%d",&n);
+	printf("Ok enter %d elements mow\n",n);
+	for(i=0;i<n;i++)
+	{
+		scanf("%d",&a[i]);
+		
+	}
+}
